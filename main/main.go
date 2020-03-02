3923cb6100a91dd3b5d1ee7a5595a9c31c5c84cf
ayuayue <1401262639@qq.com>
2020-03-02 11:41:16 +0800

    v1.1,检测目录是否有下载过得图片,有将跳过

--- main/main.go
+++ main/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //Resp 定义响应信息结构体
@@ -59,6 +60,7 @@ type Num struct {
 	Message string
 	Data    NumData
 }
+
 // NumData 图片信息
 type NumData struct {
 	AllCount   int `json:"all_count"`
@@ -69,18 +71,25 @@ type NumData struct {
 
 func main() {
 	user := new(Info)
-	fmt.Println("请输入b站up主的uid : ")
-	_, err := fmt.Scanln(&user.Uid)
-	if err != nil {
-		fmt.Println(err)
+
+	for {
+		fmt.Println("请输入b站up主的uid : ")
+		_, err := fmt.Scanln(&user.Uid)
+		if err != nil {
+			fmt.Println(err)
+		}
+		num := GetImgNum(user.Uid)
+		// fmt.Println(num)
+		if num != 0 {
+			GetSrc(user.Uid, num)
+			break
+		}
+		fmt.Println("--------------------------")
+		fmt.Println("该up主还没有上传相册哦 >_<~~")
+		time.Sleep(time.Second * 2)
+
 	}
-	num := GetImgNum(user.Uid)
-	// fmt.Println(num)
-	if num != 0 {
-		GetSrc(user.Uid, num)
-	}
-	fmt.Println("--------------------------")
-	fmt.Println("该up主还没有上传相册哦 >_<~~")
+
 }
 
 //GetSrc 获取图片链接
@@ -88,10 +97,18 @@ func GetSrc(uid int, num int) {
 	index := 1
 	pageNum := "0"
 	biz := "all"
-	err := os.Mkdir(strconv.Itoa(uid), os.ModePerm)
+	//判断文件夹是否存在
+	ok, err := IsExists(strconv.Itoa(uid))
 	if err != nil {
-		panic("创建文件夹失败")
+		panic("文件夹检测失败 >_<!")
 	}
+	if !ok {
+		err := os.Mkdir(strconv.Itoa(uid), os.ModePerm)
+		if err != nil {
+			panic("创建文件夹失败")
+		}
+	}
+
 	url := "https://api.vc.bilibili.com/link_draw/v1/doc/doc_list?uid=" + strconv.Itoa(uid) + "&page_num=" + pageNum + "&page_size=" + strconv.Itoa(num) + "&biz=" + biz
 	resp, err := http.Get(url)
 	if err != nil {
@@ -111,33 +128,41 @@ func GetSrc(uid int, num int) {
 		for _, v := range items {
 			//获取每一个picture
 			fmt.Println("正在读取第" + strconv.Itoa(index) + "个相册,共" + strconv.Itoa(num) + "个相册")
-				
+
 			for k, v1 := range v.Pictures {
-				//读取图片原始二进制数据
-				bin, err := http.Get(v1.ImgSrc)
-				if err != nil {
-					panic("读取图片数据失败 >_<!")
-				}
-				defer bin.Body.Close()
-				imgBin, err := ioutil.ReadAll(bin.Body)
-				if err != nil {
-					panic("读取图片数据失败 >_<!")
-				}
+
 				//开始写入图片
 				//获取图片的后缀,并更改名字重新保存
 				dot := strings.LastIndex(v1.ImgSrc, ".")
 				ext := v1.ImgSrc[dot:]
-				f, err := os.OpenFile(strconv.Itoa(uid)+"/"+strconv.Itoa(index)+"-"+strconv.Itoa(k+1)+ext, os.O_CREATE, 0666)
-				defer f.Close()
+				//检测文件是否存在,如果存在则跳过,不存在则写入
+				path := strconv.Itoa(uid) + "/" + strconv.Itoa(index) + "-" + strconv.Itoa(k+1) + ext
+				ok, err := IsExists(path)
 				if err != nil {
-					panic("文件读取失败 >_<!")
+					panic("文件检测错误 >_<!")
 				}
-				_, err = f.Write([]byte(imgBin))
-				if err != nil {
-					panic("文件写入失败 >_<!")
+				if !ok {
+					//读取图片原始二进制数据
+					bin, err := http.Get(v1.ImgSrc)
+					if err != nil {
+						panic("访问图片失败 >_<!")
+					}
+					defer bin.Body.Close()
+					imgBin, err := ioutil.ReadAll(bin.Body)
+					if err != nil {
+						panic("读取图片数据失败 >_<!")
+					}
+					f, err := os.OpenFile(strconv.Itoa(uid)+"/"+strconv.Itoa(index)+"-"+strconv.Itoa(k+1)+ext, os.O_CREATE, 0666)
+					defer f.Close()
+					if err != nil {
+						panic("文件读取失败 >_<!")
+					}
+					_, err = f.Write([]byte(imgBin))
+					if err != nil {
+						panic("文件写入失败 >_<!")
+					}
+					fmt.Println("正在下载第" + strconv.Itoa(index) + "个相册的第" + strconv.Itoa(k+1) + "个图片,共" + strconv.Itoa(len(v.Pictures)) + "个图片")
 				}
-			fmt.Println("正在下载第"+strconv.Itoa(index)+"个相册的第" + strconv.Itoa(k+1) + "个图片,共" + strconv.Itoa(len(v.Pictures)) + "个图片")
-			
 			}
 			index++
 		}
@@ -179,3 +204,14 @@ func GetImgNum(uid int) int {
 
 }
 
+//IsExists 检测文件是否存在
+func IsExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
